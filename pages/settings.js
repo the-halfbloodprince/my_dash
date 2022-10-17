701a5d3692498545494c66df8eb7240746a08073
Manish Kumar Das <ammanishkumardas@gmail.com>
2022-10-18 02:46:23 +0530

    settings init

---
+++ pages/settings.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div className='px-20 py-20 flex-col'>
+        <h1 className='text-5xl mb-4 '>Settings</h1>
+        <hr className='border-[#555]' />
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Settings
