d81de7033beb82474e5d707b44c7e91e6e909a2f
Manish Kumar Das <ammanishkumardas@gmail.com>
2022-10-17 19:14:26 +0530

    text-center

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <title>My Dash - Manish</title>
       </Head>
 
-      <main className='flex flex-col w-screen h-screen justify-center space-y-10 px-24 items-center'>
+      <main className='flex flex-col w-screen h-screen justify-center space-y-10 px-24 items-center text-center'>
         <p className='text-6xl text-subtitle hover:text-subtitle-glow transition-all duration-500 ease-in'>Hi <span className='text-glow'>{`${name}`}</span></p>
         <p className='text-9xl text-subtitle hover:text-subtitle-glow transition-all duration-500 ease-in'>It&apos;s <span className='text-glow text-[15vw]'>19:27</span> now. </p>
         <p className='text-6xl text-subtitle hover:text-subtitle-glow transition-all duration-500 ease-in'>Date: <span className='text-glow'>17th October 2022</span> </p>
